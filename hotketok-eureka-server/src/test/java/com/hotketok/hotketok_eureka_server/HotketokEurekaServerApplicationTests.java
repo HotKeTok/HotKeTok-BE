30dcf99101d25e96552cddb4aa3468faa26b92ce
alswjdghks <jerrymin1221@naver.com>
2025-09-11 00:26:07 +0900

    feat: eureka server 구성

---
+++ hotketok-eureka-server/src/test/java/com/hotketok/hotketok_eureka_server/HotketokEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotketok.hotketok_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotketokEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
