47fe3bb784cdb761cdabbd88e32fdfa39c78acf5
alswjdghks <jerrymin1221@naver.com>
2025-09-15 01:46:26 +0900

    feat: hotketok-common-service 모듈 구성

---
+++ hotketok-common-service/src/main/java/com/hotketok/hotketokcommonservice/HotketokCommonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotketok.hotketokcommonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotketokCommonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotketokCommonServiceApplication.class, args);
+	}
+
+}
---
+++ hotketok-common-service/src/test/java/com/hotketok/hotketokcommonservice/HotketokCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotketok.hotketokcommonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotketokCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
