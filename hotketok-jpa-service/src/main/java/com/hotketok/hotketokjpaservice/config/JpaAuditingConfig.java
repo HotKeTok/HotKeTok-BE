289a6e0a4f190121829a09e5d87a74adac804ca9
jerrymin1221 <jerrymin1221@naver.com>
2025-09-17 01:30:48 +0900

    [feat] BaseTimeEntity 생성 및 QueryDsl 설정 (#11)
    * feat: jpa-service 공통 모듈 생성 및 의존성 추가(Spring Data JPA, QueryDsl)
    
    * feat: BaseTimeEntity 생성 및 AuditingConfig 설정(상속하면 자동으로 생성날짜와 수정날짜 적용)
    
    * chore: QueryDsl 설정 추가

---
+++ hotketok-jpa-service/src/main/java/com/hotketok/hotketokjpaservice/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.hotketok.hotketokjpaservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
