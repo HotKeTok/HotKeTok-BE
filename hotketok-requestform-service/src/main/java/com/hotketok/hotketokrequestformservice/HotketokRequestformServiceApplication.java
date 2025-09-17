99e3f5e12502f5fe0577e6c52c3f2e2ce7b5cd41
alswjdghks <jerrymin1221@naver.com>
2025-09-17 16:39:08 +0900

    chore: requestform-service (요청서 서비스) 모듈 초기설정

---
+++ hotketok-requestform-service/src/main/java/com/hotketok/hotketokrequestformservice/HotketokRequestformServiceApplication.java
@@ -0,0 +1,16 @@
+package com.hotketok.hotketokrequestformservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HotketokRequestformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotketokRequestformServiceApplication.class, args);
+
+	}
+}
