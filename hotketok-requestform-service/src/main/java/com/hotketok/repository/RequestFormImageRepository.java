828815ba1be47dfb2d31f3364d9947dd28d5caa4
alswjdghks <jerrymin1221@naver.com>
2025-09-20 17:50:35 +0900

    feat: 요청서,요청서 repository 생성

---
+++ hotketok-requestform-service/src/main/java/com/hotketok/repository/RequestFormImageRepository.java
@@ -0,0 +1,7 @@
+package com.hotketok.repository;
+
+import com.hotketok.domain.RequestFormImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestFormImageRepository extends JpaRepository<RequestFormImage, Long> {
+}
