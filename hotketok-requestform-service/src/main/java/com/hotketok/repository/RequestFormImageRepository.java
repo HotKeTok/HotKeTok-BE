dbdb2ebc4b25be2821430e1eba946c374104d63d
alswjdghks <jerrymin1221@naver.com>
2025-09-21 16:33:32 +0900

    feat: 이미지 아이디들로 DB 데이터 전체 삭제

--- hotketok-requestform-service/src/main/java/com/hotketok/repository/RequestFormImageRepository.java
+++ hotketok-requestform-service/src/main/java/com/hotketok/repository/RequestFormImageRepository.java
@@ -3,5 +3,8 @@ package com.hotketok.repository;
 import com.hotketok.domain.RequestFormImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RequestFormImageRepository extends JpaRepository<RequestFormImage, Long> {
+    void deleteAllByIdIn(List<Long> RequestFormImageIds);
 }
