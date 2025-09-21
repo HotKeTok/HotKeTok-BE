de65cd170a292638d5ba68c6d32568a8c8c3be6e
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-21 13:14:57 +0900

    feat: 서비스 등록 추가

---
+++ hotketok-chatting-server/src/main/java/com/hotketok/hotketokchattingserver/HotketokChattingServerApplication.java
@@ -0,0 +1,13 @@
+package com.hotketok.hotketokchattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotketokChattingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotketokChattingServerApplication.class, args);
+	}
+
+}
---
+++ hotketok-chatting-server/src/test/java/com/hotketok/hotketokchattingserver/HotketokChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotketok.hotketokchattingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotketokChattingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
