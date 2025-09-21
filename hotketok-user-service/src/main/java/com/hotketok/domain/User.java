2b7c8866a533405a610fd0a9af033d14d28ba249
alswjdghks <jerrymin1221@naver.com>
2025-09-22 04:07:23 +0900

    feat: user 도메인 설정

---
+++ hotketok-user-service/src/main/java/com/hotketok/domain/User.java
@@ -0,0 +1,44 @@
+package com.hotketok.domain;
+
+import com.hotketok.domain.enums.Role;
+import com.hotketok.domain.enums.SocialType;
+import com.hotketok.hotketokjpaservice.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String logInId;// 로그인 ID
+
+    @Column(unique = true)
+    private String password;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    private String email;
+
+    @Column
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+}
