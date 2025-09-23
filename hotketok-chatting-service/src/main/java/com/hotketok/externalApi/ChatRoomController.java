37673c7bdad6f0f4189c671f60f9f8f9847bb57d
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-23 19:33:58 +0900

    fix: 채팅 서비스 기본 url 통일을 위한 변경

--- hotketok-chatting-service/src/main/java/com/hotketok/externalApi/ChatRoomController.java
+++ hotketok-chatting-service/src/main/java/com/hotketok/externalApi/ChatRoomController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/chatting-service")
 @RequiredArgsConstructor
 public class ChatRoomController {
 
