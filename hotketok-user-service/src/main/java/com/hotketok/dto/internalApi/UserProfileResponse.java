a0edc36bd9ef740b3f2d71e98c715e2aeb33baf1
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-23 11:25:36 +0900

    fix: 유저 서비스 role에 대한 타입 불일치 해결

--- hotketok-user-service/src/main/java/com/hotketok/dto/internalApi/UserProfileResponse.java
+++ hotketok-user-service/src/main/java/com/hotketok/dto/internalApi/UserProfileResponse.java
@@ -2,38 +2,32 @@ package com.hotketok.dto.internalApi;
 
 import com.hotketok.domain.User;
 import com.hotketok.domain.enums.Role;
-import com.hotketok.domain.enums.SenderType;
-
-import static com.hotketok.domain.enums.SenderType.HOUSE_USER;
-
 
 public record UserProfileResponse(
         Long userId,
         String userName,
         String profileImageUrl,
-        SenderType role // 채팅 서비스의 SenderType
+        String role
 ) {
-
     public static UserProfileResponse from(User user) {
         return new UserProfileResponse(
                 user.getId(),
                 user.getName(),
                 user.getProfileImage(),
-                convertRoleToSenderType(user.getRole())  // ENUM 타입 반환
+                convertRoleToString(user.getRole())
         );
     }
 
-    // 유저 서비스의 Role -> 채팅 서비스의 UserType으로 변환
-    private static SenderType convertRoleToSenderType(Role role) {
+    // 유저 서비스 role을 string으로 변환
+    private static String convertRoleToString(Role role) {
         if (role == null) {
-            return HOUSE_USER; // 기본값
+            return "HOUSE_USER";
         }
         return switch (role.name()) {
-            case "VENDOR" -> SenderType.VENDOR;
-            case "TENANT" -> SenderType.TENANT;
-//            case "OWNER" -> HOUSE_USER;
-            default -> HOUSE_USER;
+            case "VENDOR" -> "VENDOR";
+            case "TENANT" -> "TENANT";
+//            case "OWNER" -> "HOUSE_USER";
+            default -> "HOUSE_USER";
         };
     }
 }
-
