c3e1a48075a9b3129ded9e6dd077928cfcd559f3
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-24 17:10:43 +0900

    fix: 채팅 서비스 포트번호 변경

---
+++ hotketok-post-service/src/main/java/com/hotketok/HotketokPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotketok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotketokPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotketokPostServiceApplication.class, args);
+	}
+
+}
