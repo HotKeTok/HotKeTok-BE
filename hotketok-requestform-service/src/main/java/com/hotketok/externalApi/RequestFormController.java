c15583b1f8bc2104640a019f083f4c919b30926f
alswjdghks <jerrymin1221@naver.com>
2025-09-24 21:06:08 +0900

    fix: 요청서 서비스 외부 API URL Path 수정

--- hotketok-requestform-service/src/main/java/com/hotketok/externalApi/RequestFormController.java
+++ hotketok-requestform-service/src/main/java/com/hotketok/externalApi/RequestFormController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/requestform-service/")
+@RequestMapping("/api/requestform-service")
 @RequiredArgsConstructor
 public class RequestFormController {
 
