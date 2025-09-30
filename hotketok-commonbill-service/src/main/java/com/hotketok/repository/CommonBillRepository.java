e937922d5ae27fd90d8b9407f807a475a25a7be2
alswjdghks <jerrymin1221@naver.com>
2025-09-30 21:21:26 +0900

    feat: 공동관리비 repository 설정

---
+++ hotketok-commonbill-service/src/main/java/com/hotketok/repository/CommonBillRepository.java
@@ -0,0 +1,8 @@
+package com.hotketok.repository;
+
+import com.hotketok.domain.CommonBill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonBillRepository extends JpaRepository<CommonBill, Long> {
+
+}
