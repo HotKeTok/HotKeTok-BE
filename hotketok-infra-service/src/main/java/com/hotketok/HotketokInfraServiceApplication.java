f74465b74645894a5b42df17452522fe355420ce
shinminkyoung1 <dottiee0702@gmail.com>
2025-10-02 11:00:42 +0900

    refactor: env 파일 직접 설정을 통한 불필요 라인 삭제

--- hotketok-infra-service/src/main/java/com/hotketok/HotketokInfraServiceApplication.java
+++ hotketok-infra-service/src/main/java/com/hotketok/HotketokInfraServiceApplication.java
@@ -1,6 +1,5 @@
 package com.hotketok;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotketokInfraServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
 		SpringApplication.run(HotketokInfraServiceApplication.class, args);
 	}
 
