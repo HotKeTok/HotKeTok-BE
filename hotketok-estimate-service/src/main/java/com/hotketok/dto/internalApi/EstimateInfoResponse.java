3ce9c1472f5df3d69622068a7b35cde6ccee8c2a
shinminkyoung1 <dottiee0702@gmail.com>
2025-10-13 23:49:37 +0900

    refactor: 예상 시간 출력 형식 변경

--- hotketok-estimate-service/src/main/java/com/hotketok/dto/internalApi/EstimateInfoResponse.java
+++ hotketok-estimate-service/src/main/java/com/hotketok/dto/internalApi/EstimateInfoResponse.java
@@ -4,20 +4,25 @@ import com.hotketok.domain.enums.Status;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public record EstimateInfoResponse(
         Long estimateId,
         Long requestFormId,
-        LocalDateTime estimateTime,
+        String estimateTime,
         Status status,
         BigDecimal estimatePrice,
         String estimateComment
 ) {
     public static EstimateInfoResponse from(Estimate estimate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd / a hh:mm", Locale.KOREAN);
+        String formattedTime = estimate.getCreatedAt().format(formatter);
+
         return new EstimateInfoResponse(
                 estimate.getId(),
                 estimate.getRequestFormId(),
-                estimate.getCreatedAt(),
+                formattedTime,
                 estimate.getStatus(),
                 estimate.getEstimatePrice(),
                 estimate.getComment()
