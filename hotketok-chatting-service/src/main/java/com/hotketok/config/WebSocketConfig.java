b8117f1f5afe3045f1e6e0673daf6a374d5a90c0
shinminkyoung1 <dottiee0702@gmail.com>
2025-10-22 16:47:10 +0900

    fix: 채팅 서비스 내 cors 관련 설정 모두 제외

--- hotketok-chatting-service/src/main/java/com/hotketok/config/WebSocketConfig.java
+++ hotketok-chatting-service/src/main/java/com/hotketok/config/WebSocketConfig.java
@@ -13,8 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                // .setAllowedOriginPatterns("http://localhost:5173")
-                .setAllowedOriginPatterns("*") // ✅ Gateway가 CORS 처리하므로 단순 허용
                 .withSockJS();
     }
 
