f421457d92b31e9c0b84d1ba82c4deb0fc3b39d0
shinminkyoung1 <dottiee0702@gmail.com>
2025-10-25 16:39:06 +0900

    fix: 삭제 항목 복원

--- hotketok-review-service/src/main/java/com/hotketok/dto/ReviewItemResponse.java
+++ hotketok-review-service/src/main/java/com/hotketok/dto/ReviewItemResponse.java
@@ -1,7 +1,12 @@
 package com.hotketok.dto;
 
 import com.hotketok.domain.Review;
+import com.hotketok.domain.ReviewImage;
 import com.hotketok.dto.internalApi.UserProfileResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public record ReviewItemResponse(
         Long reviewId,
@@ -9,11 +14,18 @@ public record ReviewItemResponse(
         String category,
         String content,
         String writerName,
-        String writerProfileImage
+        String writerProfileImage,
+        LocalDateTime date,
+        List<String> reviewImage,
+        Long authorId
 ) {
     public static ReviewItemResponse of(Review review, UserProfileResponse writerProfile) {
         String name = (writerProfile != null) ? writerProfile.userName() : "알 수 없는 사용자";
         String image = (writerProfile != null) ? writerProfile.profileImageUrl() : null;
+
+        List<String> imageUrls = review.getReviewImages().stream()
+                .map(ReviewImage::getImageUrl)
+                .collect(Collectors.toList());
 
         return new ReviewItemResponse(
                 review.getId(),
@@ -21,7 +33,10 @@ public record ReviewItemResponse(
                 review.getConstructCategory().name(),
                 review.getReview(),
                 name,
-                image
+                image,
+                review.getCreatedAt(),
+                imageUrls,
+                review.getUserId()
         );
     }
 }
