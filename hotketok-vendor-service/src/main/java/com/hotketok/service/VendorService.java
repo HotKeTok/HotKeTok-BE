222e98c11674114d59e2be1b623e9b4ce7e39a7b
shinminkyoung1 <dottiee0702@gmail.com>
2025-10-25 17:28:47 +0900

    fix: timeformatter 제외

--- hotketok-vendor-service/src/main/java/com/hotketok/service/VendorService.java
+++ hotketok-vendor-service/src/main/java/com/hotketok/service/VendorService.java
@@ -423,12 +423,10 @@ public class VendorService {
         RequestFormDetailResponse formData = requestFormServiceClient.getRequestFormDetail(requestId);
         UserInfoDetailResponse payerInfo = userServiceClient.getUserInfoById(formData.payerId());
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd / a hh:mm", Locale.KOREAN);
-
         return new RequestDetailResponse(
                 formData.category(),
                 formData.address(),
-                formData.requestSchedule().format(formatter),
+                formData.requestSchedule(),
                 formData.payType(),
                 payerInfo.name(),
                 payerInfo.phoneNumber(),
