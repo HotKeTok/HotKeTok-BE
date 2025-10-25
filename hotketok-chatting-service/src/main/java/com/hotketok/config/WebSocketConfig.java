0a3e68395187d96604cba8f4acdc1f1842e8828c
Minkyoung Shin <151979379+shinminkyoung1@users.noreply.github.com>
2025-10-25 17:51:23 +0900

    fix: config 허용 url 제거 (#128)

--- hotketok-chatting-service/src/main/java/com/hotketok/config/WebSocketConfig.java
+++ hotketok-chatting-service/src/main/java/com/hotketok/config/WebSocketConfig.java
@@ -13,7 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("http://localhost:5173", "https://hotketok.shop")
                 .withSockJS()
                 .setSessionCookieNeeded(true);
     }
